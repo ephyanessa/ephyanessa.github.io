a66eaf557e35221f31087d6d0de40ccac63f67c2
jamzrob <raubenheimerjames@gmail.com>
2018-06-12 17:00:23 +0000

    Added time selection

--- asset/Js/select.js
+++ asset/Js/select.js
@@ -4,10 +4,13 @@ function buttonClicked(clicked_id)
   document.getElementById("selected-seat").innerHTML=clicked_id;
 }
 
-function routeClick(clicked_id,fare)
+function routeClick(clicked_id,fare,destination_time)
 {
   document.getElementById("selected-destination").innerHTML=clicked_id;
   document.getElementById("selected-fare").innerHTML= fare + " cedis";
+  var time = document.getElementById(destination_time);
+  var index=time.selectedIndex;
+  document.getElementById("selected-time").innerHTML=time.options[index].text;
 }
 /*function nameOfFunction(parameter)
 {
