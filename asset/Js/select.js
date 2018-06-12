46ae571c986c94cf48e1b96a9120e065faca6ee9
van otchere <ephyanessa@gmail.com>
2018-06-12 15:41:25 +0100

    merged files

--- asset/Js/select.js
+++ asset/Js/select.js
@@ -3,3 +3,12 @@ function buttonClicked(clicked_id)
 
   document.getElementById("selected-seat").innerHTML=clicked_id;
 }
+
+function routeClick(clicked_id)
+{
+  document.getElementById("selected-destination").innerHTML=clicked_id;
+}
+/*function nameOfFunction(parameter)
+{
+  does something
+}*/
