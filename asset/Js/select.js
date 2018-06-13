3415020f91ab1116581705f15fd42f42f7904a3f
van otchere <ephyanessa@gmail.com>
2018-06-13 15:11:51 +0100

    added the companies and an image

--- asset/Js/select.js
+++ asset/Js/select.js
@@ -4,13 +4,16 @@ function buttonClicked(clicked_id)
   document.getElementById("selected-seat").innerHTML=clicked_id;
 }
 
-function routeClick(clicked_id,fare,destination_time)
+function routeClick(clicked_id,fare,destination_time,destination_company)
 {
   document.getElementById("selected-destination").innerHTML=clicked_id;
   document.getElementById("selected-fare").innerHTML= fare + " cedis";
   var time = document.getElementById(destination_time);
   var index=time.selectedIndex;
   document.getElementById("selected-time").innerHTML=time.options[index].text;
+  var company = document.getElementById(destination_company);
+  var index=company.selectedIndex;
+  document.getElementById("selected-company").innerHTML=company.options[index].text;
 }
 /*function nameOfFunction(parameter)
 {
