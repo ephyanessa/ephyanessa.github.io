cfd8ce668b448a1c73f0dcd67dd5d15849cfaf59
jamzrob <raubenheimerjames@gmail.com>
2018-06-14 15:36:25 +0000

    Added ticket creation

--- asset/Js/select.js
+++ asset/Js/select.js
@@ -15,6 +15,28 @@ function routeClick(clicked_id,fare,destination_time,destination_company)
   var index=company.selectedIndex;
   document.getElementById("selected-company").innerHTML=company.options[index].text;
 }
+
+// init the jsPDF library
+const pdf = new jsPDF();
+// select the button
+let button = document.getElementById('button-ticket');
+// select the input
+let input = document.getElementById('input-ticket').value;
+
+// actual PDF options
+function printPDF() {
+		// @param 1 - Coordinate (in units declared at inception of PDF document) against left edge of the page
+		// @param 2 - Coordinate (in units declared at inception of PDF document) against upper edge of the page
+		// @param 3 - String or array of strings to be added to the page. Each line is shifted one line down per font, spacing settings declared before this call.
+		pdf.text(10, 10, `You have to pay ${input.value}$`);
+			     // save the PDF document (downloadable)
+			    pdf.save();
+		}
+
+function ticketSelected() {
+  printPDF();
+}
+
 /*function nameOfFunction(parameter)
 {
   does something
